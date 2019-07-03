fb1734be9f6e60ca5d3a31a5414ef5e538f39ef3
eapu <elena.andresprado@gmail.com>
2019-07-03 15:16:42 +0100

    Heroku

--- mystylo/urls.py
+++ mystylo/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
+admin.site.site_header = 'My space'                    # default: "Django Administration"
+admin.site.index_title = 'Administration'                 # default: "Site administration"
+admin.site.site_title = 'Administration' # default: "Django site admin"
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('styloxxi.urls')),
